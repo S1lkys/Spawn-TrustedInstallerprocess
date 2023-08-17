9b0bf59c7a1f100f4580c20098b51260b73cf038
Silky <40408435+S1lkys@users.noreply.github.com>
2023-08-17 15:37:14 +0200

    Create Spawn_ntsystem_trustedinstaller.ps1
    Needs Admin rights

---
+++ Spawn_ntsystem_trustedinstaller.ps1
@@ -0,0 +1,17 @@
+$bytesAPI = [System.IO.File]::ReadAllBytes('C:\Users\user\Downloads\NtApiDotNet.dll')
+$bytesObjM = [System.IO.File]::ReadAllBytes('C:\Users\user\Downloads\NtObjectManager.dll')
+$d = [System.Reflection.Assembly]::Load($bytesAPI)
+$e = [System.Reflection.Assembly]::Load($bytesObjM)
+Import-Module $d
+Import-Module $e
+
+$Token= Get-NtToken -Primary
+$Token.SetPrivilege([NtApiDotNet.TokenPrivilegeValue[]]"SeDebugPrivilege", [NtApiDotNet.PrivilegeAttributes]"Enabled")
+$Token.Privileges | Where-Object {$_.name -eq "SeDebugPrivilege"}
+Start-Service Trustedinstaller
+$ti_process=  Get-NtProcess -Name "TrustedInstaller.exe"
+$config = New-Object NtApiDotNet.Win32.Win32ProcessConfig
+$config.CommandLine = "powershell"
+$config.CreationFlags = [NtApiDotNet.Win32.CreateProcessFlags]16
+$config.ParentProcess = $ti_process
+[NtApiDotNet.Win32.Win32Process]::CreateProcess($config)
