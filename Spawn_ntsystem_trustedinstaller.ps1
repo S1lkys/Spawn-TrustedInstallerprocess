748315749969ce12870671a6306cabd3c0a8209f
Silky <40408435+S1lkys@users.noreply.github.com>
2025-10-18 00:40:28 +0200

    Update Spawn_ntsystem_trustedinstaller.ps1

--- Spawn_ntsystem_trustedinstaller.ps1
+++ Spawn_ntsystem_trustedinstaller.ps1
@@ -9,9 +9,9 @@ $Token= Get-NtToken -Primary
 $Token.SetPrivilege([NtApiDotNet.TokenPrivilegeValue[]]"SeDebugPrivilege", [NtApiDotNet.PrivilegeAttributes]"Enabled")
 $Token.Privileges | Where-Object {$_.name -eq "SeDebugPrivilege"}
 Start-Service Trustedinstaller
-$ti_process=  Get-NtProcess -Name "TrustedInstaller.exe"
+$ti_process = Get-NtProcess -Name "TrustedInstaller.exe" | Select-Object -First 1
 $config = New-Object NtApiDotNet.Win32.Win32ProcessConfig
 $config.CommandLine = "powershell"
-$config.CreationFlags = [NtApiDotNet.Win32.CreateProcessFlags]16
+$config.CreationFlags = [NtApiDotNet.Win32.CreateProcessFlags]::NewConsole
 $config.ParentProcess = $ti_process
 [NtApiDotNet.Win32.Win32Process]::CreateProcess($config)
